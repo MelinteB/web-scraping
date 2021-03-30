67f713dfd251c8220a7834511814becd36190f07
MelinteB <melinte.bogdan@yahoo.ro>
2021-03-30 20:54:13 +0300

    accounts app

---
+++ README.md
@@ -0,0 +1,7 @@
+# web-scraping
+## Description
+Fetch data from web page and save them to a database
+
+##Features
+ * user administration( Login, Logout, Sign-In, Sign-Out )
+ * homepage (search, top real-estates )
---
+++ web_scraping/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import LoginViewCustom
+
+app_name='accounts'
+urlpatterns = [
+    path('login/',LoginViewCustom.as_view(), name='login'),
+]
